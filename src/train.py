7fa26718e86f9d0f347a8c8d13d27ea04b1c6901
Jean Yves Esmel <ajy.esmel@gmail.com>
2026-03-30 08:56:31 +0000

    Corretion d'une syntaxe

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_reprot, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 from preprocessing import chargement_des_donnees, remplacer_les_donnees_manquantes, selection_des_variables, normalisation_des_donnees
 
